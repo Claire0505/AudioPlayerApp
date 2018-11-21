636326583f40fdcdaebbe64d6f99a49058041084
Claire <claire0505@gmail.com>
2018-11-21 22:06:12 +0800

    1.在AndroidManifest.xml添加權限 MEDIA_CONTENT_CONTROL 控制娛體播放，使用READ_PHONE_STATE權限 訪問電話狀態以收聽來電等事件，這樣就可以在通話過程中停止音頻。  <uses-permission android:name="android.permission.INTERNET"/>     <permission android:name="android.permission.MEDIA_CONTENT_CONTROL"/>     <uses-permission android:name="android.permission.READ_PHONE_STATE"/>     <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/> 2.在MainActivity.java 添加權限方法 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE )                 != PackageManager.PERMISSION_GRANTED &&                ActivityCompat.checkSelfPermission(this,  Manifest.permission.READ_EXTERNAL_STORAGE)                 != PackageManager.PERMISSION_GRANTED ){             //未取得權限，向使用者要求允許權限             ActivityCompat.requestPermissions(this,                     new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE},                     REQUEST_PERMISSION);
            }
    
     // 選擇完後覆寫方法
        @Override
        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
            switch (requestCode){
                case REQUEST_PERMISSION:
                    if (grantResults.length > 0 &&
                            grantResults[0] == PackageManager.PERMISSION_GRANTED){
                        //取得權限
                    }
            }
        }

--- app/src/main/java/com/claire/audioplayerapp/MainActivity.java
+++ app/src/main/java/com/claire/audioplayerapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.claire.audioplayerapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,10 +15,24 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSION = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE )
+                != PackageManager.PERMISSION_GRANTED &&
+               ActivityCompat.checkSelfPermission(this,  Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ){
+            //未取得權限，向使用者要求允許權限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    // 選擇完後覆寫方法
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_PERMISSION:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //取得權限
+                }
+        }
     }
 
     @Override
